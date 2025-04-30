3d2d3901248782ae60a4ff6b3e890b09ab56f47d
Anthony Manning-Franklin <anthony.manning.franklin@gmail.com>
2025-04-30 21:16:34 +0800

    improvements

--- src/talon/dispatchCommand.ts
+++ src/talon/dispatchCommand.ts
@@ -1,23 +1,23 @@
-import { Command } from "@tauri-apps/plugin-shell";
+import { Command } from '@tauri-apps/plugin-shell';
 
 let priorCmdPromise: Promise<unknown> = Promise.resolve();
 
 export const dispatchCommand = async (command: string) => {
-    const args = ['-c', `echo '${command}' | ~/.talon/bin/repl`];
-    const cmd = Command.create('talon-rpc', args);
-    await awaitSafely(priorCmdPromise);
-    const cmdPromise = cmd.execute();
-    priorCmdPromise = cmdPromise;
-    const { signal, stderr, stdout } = await cmdPromise;
-    const errText = stdout.split('\n')[1];
-    if (errText) console.warn(errText);
-    if (signal) console.error(stderr);
-}
+  const args = ['-c', `echo '${command}' | ~/.talon/bin/repl`];
+  const cmd = Command.create('talon-rpc', args);
+  await awaitSafely(priorCmdPromise);
+  const cmdPromise = cmd.execute();
+  priorCmdPromise = cmdPromise;
+  const { signal, stderr, stdout } = await cmdPromise;
+  const errText = stdout.split('\n')[1];
+  if (errText) console.warn(errText);
+  if (signal) console.error(stderr);
+};
 
 const awaitSafely = async (promise: Promise<unknown>) => {
-    try {
-        await promise;
-    } catch(error) {
-        console.error(error)
-    }
-}
+  try {
+    await promise;
+  } catch (error) {
+    console.error(error);
+  }
+};
--- src/talon/initTalonPolling.ts
+++ src/talon/initTalonPolling.ts
@@ -1,18 +1,25 @@
-import { dispatchCommand } from "./dispatchCommand";
-import { memoise } from "../util/memoise";
-import { initTalonEventListener, readTalonEvents } from "./readTalonEvents";
-import { Func } from "../util/Func";
+import { dispatchCommand } from './dispatchCommand';
+import { memoise } from '../util/memoise';
+import { initTalonEventListener, readTalonEvents } from './readTalonEvents';
+import { Func } from '../util/Func';
 
 export const initTalonPolling = memoise(async () => {
   await dispatchCommand('actions.user.sync_talon_tray_state()');
   await readTalonEvents();
   await initTalonEventListener();
   setInterval(runTick, 35);
-
+  setInterval(
+    () => dispatchCommand('actions.user.sync_talon_tray_state()'),
+    10_000
+  );
 });
 
 const polledFuncs = new Set<Func>();
-const runTick = () => { for (const fn of polledFuncs) { fn() } }
+const runTick = () => {
+  for (const fn of polledFuncs) {
+    fn();
+  }
+};
 
 export const onPollTick = <Fn extends Func>(fn: Fn) => polledFuncs.add(fn);
 export const offPollTick = <Fn extends Func>(fn: Fn) => polledFuncs.delete(fn);
